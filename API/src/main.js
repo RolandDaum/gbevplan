71f6fe7359350d840f497f3b01f7f13da3d2e2ea
OMG_OT <monsterroland7@gmail.com>
2024-03-28 14:41:14 +0100

    idk

--- API/src/main.js
+++ API/src/main.js
@@ -3,31 +3,18 @@ import * as https from 'https';
 import express from 'express';
 
 var credentials = {
-    key: fs.readFileSync('E:/gbevplan/API/certificates/gbevplanapi.key', 'utf-8'), 
-    cert: fs.readFileSync('E:/gbevplan/API/certificates/gbevplanapi.csr', 'utf-8')
+    key: fs.readFileSync('E:/gbevplan/API/certificates/private.key', 'utf-8'), 
+    cert: fs.readFileSync('E:/gbevplan/API/certificates/certificate.crt', 'utf-8')
+    // key: fs.readFileSync('E:/gbevplan/API/certificates/gbevplanapi.key', 'utf-8'), 
+    // cert: fs.readFileSync('E:/gbevplan/API/certificates/gbevplanapi.csr', 'utf-8')
 }
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('https I hope');
 })
 
-var httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, () => {
-    console.log('started server on ' + port)
+var httpsServer = https.createServer(credentials, app).listen(3000, () => {
+    console.log('started server on ' + 3000)
 });
-
-
-
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-  
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })  
