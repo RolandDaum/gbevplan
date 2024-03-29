42cd40fcd71a59f274f300ebfb88d2183f2abd50
OMG_OT <monsterroland7@gmail.com>
2024-03-29 14:29:32 +0100

    added basic api layout; Next: Selenium integration + /data Folder

--- API/src/cert.js
+++ API/src/cert.js
@@ -1,15 +1,8 @@
 import express from 'express';
 
-const response = '2A6E6CC917BB68678413ADB7E77B13C810B0EA50F6B1A8DBFB9E0845CECE9A6A\ncomodoca.com\na6e711cd1227e66';
-
 const app = express();
 
 const port = 3000;
-
-
-// app.get('/.well-known/pki-validation/', (req, res) => {
-//     // res.sendFile('E:/gbevplan/API/src/887B6BCAEE2D068631CED7E9BA907926.txt')
-// });
 
 app.use(express.static('./src'));
 
--- API/src/main.js
+++ API/src/main.js
@@ -1,20 +1,109 @@
 import * as fs from 'fs';
 import * as https from 'https';
 import express from 'express';
+import { Builder, Browser, By, Key, until } from 'selenium-webdriver';
 
-var credentials = {
-    key: fs.readFileSync('E:/gbevplan/API/certificates/private.key', 'utf-8'), 
-    cert: fs.readFileSync('E:/gbevplan/API/certificates/certificate.crt', 'utf-8')
-    // key: fs.readFileSync('E:/gbevplan/API/certificates/gbevplanapi.key', 'utf-8'), 
-    // cert: fs.readFileSync('E:/gbevplan/API/certificates/gbevplanapi.csr', 'utf-8')
+const resText = {
+    400: 'missing header',
+    401: 'unauthorized',
+    404: 'content not found'
+}
+const jahrgangsPath = {
+    12: './src/data/Jahrgang12.json',
+    13: './src/data/Jahrgang13.json'
 }
 
+const credentials = {
+    key: fs.readFileSync('./certificates/private.key', 'utf-8'), 
+    cert: fs.readFileSync('./certificates/certificate.crt', 'utf-8')
+}
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('https I hope');
+app.get('/verify', (req, res) => {
+    const verify = verifyReq(req, res);
+    switch (verify) {
+        case 200:
+            res.status(verify);
+            res.send('verified');
+            break;
+    }
+});
+// checks username + password and autoresponse if wrong or missing, else return 200
+function verifyReq(req, res) {
+    const reqUsername = req.headers.username;
+    const reqPassword = req.headers.password;
+
+    if (reqUsername == process.env.apiUSERNAME && reqPassword == process.env.apiPASSWORD) {
+        return 200;
+    } else {
+        if (reqUsername == null || reqPassword == null) {
+            res.status(400);
+            res.send(resText[400]);
+            return 400;
+        } else {
+            res.status(401);
+            res.send(resText[401]);
+            return 401
+        }
+    }
+}
+
+app.get('/jahrgangsplan', (req, res) => {
+    if (verifyReq(req, res) != 200) {}
+
+    const jahrgang = req.headers.jahrgang;
+    if (jahrgang == null) {
+        res.status(400);
+        res.send(resText[400]);
+        return;
+    }
+
+    try {
+        fs.readFile(jahrgangsPath[jahrgang], 'utf8', (err, data) => {
+            res.status(200);
+            res.type('json');
+            res.send(data);
+        })
+    } catch (error) {
+        res.status(404);
+        res.send(resText[404]);
+    }
+
+});
+
+app.get('/news', (req, res) => {
+    if (verifyReq(req, res) != 200) {}
+    res.status(200);
+    res.send('news');
 })
 
-var httpsServer = https.createServer(credentials, app).listen(3000, () => {
-    console.log('started server on ' + 3000)
+app.get('/vplan', (req, res) => {
+    if (verifyReq(req, res) != 200) {}
+    res.status(200);
+
+    const jahrgang = req.headers.jahrgang;
+    if (jahrgang == null) {
+        res.status(400);
+        res.send(resText[400]);
+        return;
+    }
+    vplanSrcaping();
+    res.send(jahrgang);
+})
+
+var httpsServer = https.createServer(credentials, app).listen(PORT, () => {
+    console.log('Started API Server on port: ' + PORT);
 });
+
+
+
+async function vplanSrcaping() {
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    try {
+        await driver.get('https://www.google.com/ncr')
+        console.log(await driver.getTitle())
+      } finally {
+        await driver.quit()
+      }
+}
