7691a4fa6bafecfca8f94a940fd56ba13b5f320d
OMG_OT <monsterroland7@gmail.com>
2024-04-02 15:44:26 +0200

    added script to get uuid of all timetable and parse them into url

--- API/src/vplanScraping.js
+++ API/src/vplanScraping.js
@@ -1,20 +1,65 @@
 import { Builder, Browser, By, Key, until } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js'
+
+main();
+
+async function main() {
+    const uris = await getVplanURL(1);
+    await getVPlanJson(uris);
+};
+
+async function getVplanURL(attemps) {
+    // Set up Chrome options
+    const chromeOptions = new chrome.Options();
+    chromeOptions.addArguments('--headless'); // optional: run headless
+    chromeOptions.addArguments('--silent');
+
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .setChromeOptions(chromeOptions)
+        .build();
 
 
-vplanSrcaping();
+    await driver.get(`https://www.dsbmobile.de/Login.aspx?U=${process.env.apiUSERNAME}&P=${process.env.apiPASSWORD}`).catch( (error) => {
+        console.log(error);
+    });
 
+    let vplanURL = [];
+    const elements = await driver.findElements(By.css('[data-uuid]'));
 
-const vplanURL = [];
+    for (let i = 0; i < elements.length; i++) {
+        const uuid = await elements.at(i).getAttribute('data-uuid');
+        vplanURL.push(`https://dsbmobile.de/data/a2223098-b91f-4526-b48d-55af00305b46/${uuid}/${uuid}.htm`);
+    }
 
+    driver.quit();
 
-async function vplanSrcaping() {
-    let driver = await new Builder().forBrowser(Browser.CHROME).build();
-
-    await driver.get(`https://www.dsbmobile.de/Login.aspx?U=${process.env.apiUSERNAME}&P=${process.env.apiPASSWORD}`)
-    
-    console.log(await driver.getTitle())
-
-
-    // driver.manage().setTimeouts({ implicit: 20000 });
-    // await driver.quit()
+    if (vplanURL.length <= 0 && attemps <= 10) {
+        return await getVplanURL(attemps+1)
+    } else {
+        console.log(`Attemps: ${attemps}`);
+        return vplanURL;
+    }
 }
+
+async function getVPlanJson(uriList) {
+    // Set up Chrome options
+    const chromeOptions = new chrome.Options();
+    chromeOptions.addArguments('--headless'); // optional: run headless
+    chromeOptions.addArguments('--silent');
+
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .setChromeOptions(chromeOptions)
+        .build();
+
+    uriList.forEach(async (url) => {
+        await driver.get(url).catch( (error) => {
+            console.log(error);
+        });
+        
+        const title = await driver.getCurrentUrl();
+
+        console.log('TT: ' + title);
+    })
+};
