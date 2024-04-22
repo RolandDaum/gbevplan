883234d695d07ed170005d258caa34c31edadd0f
RolandDaum <monsterroland7@gmail.com>
2024-04-22 12:44:46 +0200

    working nodejs url fetch algo

--- API/src/vplanScraping.js
+++ API/src/vplanScraping.js
@@ -4,11 +4,14 @@ import chrome from 'selenium-webdriver/chrome.js'
 main();
 
 async function main() {
-    const uris = await getVplanURL(1);
-    await getVPlanJson(uris);
+    const uris = await getVplanURL(1, []);
+    console.log(uris);
 };
 
-async function getVplanURL(attemps) {
+async function getVplanURL(attemps, vplanURLList) {
+    if (vplanURLList == null) {
+        vplanURLList = [];
+    }
     // Set up Chrome options
     const chromeOptions = new chrome.Options();
     chromeOptions.addArguments('--headless'); // optional: run headless
@@ -19,47 +22,47 @@ async function getVplanURL(attemps) {
         .setChromeOptions(chromeOptions)
         .build();
 
-
+    console.log([process.env.apiUSERNAME, process.env.apiPASSWORD])
     await driver.get(`https://www.dsbmobile.de/Login.aspx?U=${process.env.apiUSERNAME}&P=${process.env.apiPASSWORD}`).catch( (error) => {
         console.log(error);
     });
 
-    let vplanURL = [];
     const elements = await driver.findElements(By.css('[data-uuid]'));
-
     for (let i = 0; i < elements.length; i++) {
         const uuid = await elements.at(i).getAttribute('data-uuid');
-        vplanURL.push(`https://dsbmobile.de/data/a2223098-b91f-4526-b48d-55af00305b46/${uuid}/${uuid}.htm`);
+        // console.log(uuid);
+        vplanURLList.push(`https://dsbmobile.de/data/a2223098-b91f-4526-b48d-55af00305b46/${uuid}/${uuid}.htm`);
+        // console.log(vplanURLList);
     }
 
     driver.quit();
 
-    if (vplanURL.length <= 0 && attemps <= 10) {
+    if (vplanURLList.length <= 0 && attemps <= 10) {
         return await getVplanURL(attemps+1)
     } else {
         console.log(`Attemps: ${attemps}`);
-        return vplanURL;
+        return vplanURLList;
     }
 }
 
-async function getVPlanJson(uriList) {
-    // Set up Chrome options
-    const chromeOptions = new chrome.Options();
-    chromeOptions.addArguments('--headless'); // optional: run headless
-    chromeOptions.addArguments('--silent');
+// async function getVPlanJson(uriList) {
+//     // Set up Chrome options
+//     const chromeOptions = new chrome.Options();
+//     chromeOptions.addArguments('--headless'); // optional: run headless
+//     chromeOptions.addArguments('--silent');
 
-    let driver = await new Builder()
-        .forBrowser(Browser.CHROME)
-        .setChromeOptions(chromeOptions)
-        .build();
+//     let driver = await new Builder()
+//         .forBrowser(Browser.CHROME)
+//         .setChromeOptions(chromeOptions)
+//         .build();
 
-    uriList.forEach(async (url) => {
-        await driver.get(url).catch( (error) => {
-            console.log(error);
-        });
+//     uriList.forEach(async (url) => {
+//         await driver.get(url).catch( (error) => {
+//             console.log(error);
+//         });
         
-        const title = await driver.getCurrentUrl();
+//         const title = await driver.getCurrentUrl();
 
-        console.log('TT: ' + title);
-    })
-};
+//         console.log('TT: ' + title);
+//     })
+// };
