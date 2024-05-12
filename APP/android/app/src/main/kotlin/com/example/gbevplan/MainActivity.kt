13e195790310690f15831630ce76ddc1298ed631
OMG_OT <monsterroland7@gmail.com>
2024-05-12 19:41:53 +0200

    accetendly leaked an api key. Ups

--- APP/android/app/src/main/kotlin/com/example/gbevplan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gbevplan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- APP/lib/theme/sizes.dart
+++
@@ -1,6 +0,0 @@
-class AppSizes {
-  static double BorderRadiusSmall = 5;
-  static double BorderRadiusNormal = 7.5;
-  static double BorderRadiusLarge = 10;
-
-}
